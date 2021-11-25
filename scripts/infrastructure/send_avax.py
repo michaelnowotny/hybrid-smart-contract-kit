bf41eecb87679338d9b0f5596a87a5fef7051318
michael <nowotnym@gmail.com>
2021-11-25 11:55:02 -0800

    - added script to send AVAX to accounts

--- scripts/infrastructure/send_avax.py
+++ scripts/infrastructure/send_avax.py
@@ -4,21 +4,24 @@ from brownie import accounts, network, config
 
 
 def main():
-    # if not network.is_connected():
-    #     network.connect('local')
-
     print(f'{network.is_connected()}')
     print(f'{network.show_active()=}')
 
-    # dev = accounts.add(config['wallets']['dev_account_0'])
-    # chainlink_node_account_address = os.getenv('CHAINLINK_NODE_ACCOUNT_ADDRESS')
-    # if chainlink_node_account_address is None:
-    #     raise ValueError('Environmnet variable CHAINLINK_NODE_ACCOUNT_ADDRESS not set.')
-    #
-    # print(f'Funding Chainlink node account at {chainlink_node_account_address}')
-    #
-    # dev.transfer(chainlink_node_account_address, "1000 ether")
-    #
-    # # Set fulfillment permission
-    # oracle_contract = Oracle[-1]
-    # oracle_contract.setFulfillmentPermission(chainlink_node_account_address, True, {'from': dev})
+    if network.show_active() != 'avax-avash':
+        raise ValueError("Wrong network. Please run with option '--network avax-avash'!")
+
+
+    dev = accounts.add(config['networks']['avax-avash']['ava_dev_account'])
+    balance = dev.balance() / 10**18
+    print(f'{balance=}')
+
+    receiving_addresses = (
+        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+        "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
+        "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc",
+        "0x90f79bf6eb2c4f870365e785982e1f101e93b906",
+        "0x15d34aaf54267db7d7c367839aaf71a00a2c6a65"
+    )
+
+    for receiving_address in receiving_addresses:
+        dev.transfer(receiving_address, "10000 ether")
